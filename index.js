62060786b024c1084668f7d5e25a5898875c2087
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2022-12-29 15:26:09 +0530

    setting up view engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views') // Note we can use ./views instead of path.join function
+
 app.use('/', require('./routes/index'))
 app.use('/profile', require('./routes/user'))
 
