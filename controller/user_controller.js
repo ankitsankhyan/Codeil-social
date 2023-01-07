b5710014e5235883db33bb181150865e909c0901
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-01-08 01:07:31 +0530

    user converted to async and await

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -5,145 +5,109 @@ const posts = require('../model/post');
 
 
 
-module.exports.profile = function(req, res){
-   
+module.exports.profile = async function (req, res) {
 
-    // without prepopulate
+    try {
+        var post_user = await posts.find({ user: req.user._id }).populate('user');
+        return res.render('user', {
+            title: 'Profile ',
+            user: req.user,
+            local: res.locals,
+            post: post_user,
 
-
-    // posts.find({user: req.user._id}, function(err, post_user){
-    //     if(err){
-    //         console.log('error in finding posts');
-    //         return;
-    //     }
-    //  console.log(post_user[0], 'in find')
-    //     return res.render('user',{
-    //         title: 'Profile ',
-    //         user: req.user,
-    //         local: res.locals,
-    //         post: post_user
-    //     });
-    // });
-   
-    //  we will prefetch the data of user from data base whose id is given here
-  
-
-    posts.find({user: req.user._id}).populate('user').exec(function(err,post_user){
-        if(err){
-                    console.log('error in finding posts');
-                    return;
-                }
-         
-                return res.render('user',{
-                    title: 'Profile ',
-                    user: req.user,
-                    local: res.locals,
-                    post: post_user,
-                    // content : 'this is my first project'
-                });
-    })
-
-   
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
-     
-   module.exports.random_id =async function(req, res){
 
-   var found_user = await user.findById(req.params.id);
-   
-   
- 
+module.exports.random_id = async function (req, res) {
 
-    var post_user = await posts.find({user: found_user.id});
-        
-        
-    return res.render('user',{
-              title: 'Profile ',
-              user: found_user,
-              local:res.locals,
-              post: post_user
-          });
-    
-  
-   }
+    var found_user = await user.findById(req.params.id);
+    var post_user = await posts.find({ user: found_user.id });
+    return res.render('user', {
+        title: 'Profile ',
+        user: found_user,
+        local: res.locals,
+        post: post_user
+    });
 
-module.exports.favarate_sport = function(req, res){
+
+}
+
+module.exports.favarate_sport = function (req, res) {
     res.end('<h1> Cricket is my fav game <h1>');
 }
 
 
-module.exports.signup = function(req, res){
-    return res.render('sign_up.ejs',{
-          title:'sign-up page',
-          local: res.locals
+module.exports.signup = function (req, res) {
+    return res.render('sign_up.ejs', {
+        title: 'sign-up page',
+        local: res.locals
     })
 }
 
-module.exports.signIn = function(req,res){
-    return res.render('sign_in',{
+module.exports.signIn = function (req, res) {
+    return res.render('sign_in', {
         title: "Codiel| sign-in",
         local: res.locals
     });
 }
 
-module.exports.create = function(req, res){
-   
-   
+module.exports.create = async function (req, res) {
 
-    if(req.body.password != req.body.confirm_password){
-    return res.redirect('back');
-  }
-    user.findOne({email:req.body.email}, function(err, User){
-           if(err){
-            console.log('error in finding');
-            return;
-           }
-      
-        if(!User){
-            user.create({
-                email: req.body.email,
-                password: req.body.password,
-                name: req.body.name,
-                
 
-            }, function(err, user){
-                if(err){
-                    console.log('error in creating user');
-                    return;
-                }
 
-                return res.redirect('/user/sign-in');
-            })
-        }else{
-            console.log('user exists already');
-            return res.redirect('/home');
-        }
-    });
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+    var User = await user.findOne({ email: req.body.email });
+    console.log(User);
+    if (!User) {
+
+        // if user does not exist then create user
+
+        var created_user = await user.create({
+            email: req.body.email,
+            password: req.body.password,
+            name: req.body.name,
+        });
+
+        return res.redirect('/user/sign-in');
+
+    } else {
+
+        console.log('user exists already');
+        return res.redirect('/home');
+
+    }
+
 
 }
 
 
-module.exports.createSession = function(req, res){
+module.exports.createSession = function (req, res) {
     // to do later
-     return res.redirect('/user/profile');
+    return res.redirect('/user/profile');
 }
 
-module.exports.destroySession = function(req,res){
-    req.logout(function(err) {
+module.exports.destroySession = function (req, res) {
+    req.logout(function (err) {
         if (err) { return next(err); }
         res.redirect('/home');
-      });
+    });
 }
 
-module.exports.update = function(req, res){
-     if(req.user.id != req.params.id){
+module.exports.update = function (req, res) {
+    if (req.user.id != req.params.id) {
         return res.status(401).send('unauthorised');
         // this will print that this attempt was an unauthorise attempt to change
-     }
+    }
 
-     user.findByIdAndUpdate(req.params.id , req.body, function(err, update_user){
+    user.findByIdAndUpdate(req.params.id, req.body, function (err, update_user) {
         console.log(update_user);
         return res.redirect('back');
-     });
-      
-     
+    });
+
+
 }
