165891783da20c259e372393d8cfed13ceed61cd
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-01-14 12:14:28 +0530

    setup multer

--- model/user.js
+++ model/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatar');
 const schema = new mongoose.Schema({
     email:{
         type: String,
@@ -18,7 +20,9 @@ const schema = new mongoose.Schema({
         type:String,
         required:true
     },
-     
+     avatar:{
+      type: String,
+     }
 },
 {
     timestamps:true
@@ -27,6 +31,23 @@ const schema = new mongoose.Schema({
 );
 
 // note user_schema is the name given to the schema
+
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname,'..' ,AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+// static methods will make publically available
+
+schema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+schema.statics.avatarPath = AVATAR_PATH;
+
 
 const user_schema = mongoose.model('user_schema', schema);
 
