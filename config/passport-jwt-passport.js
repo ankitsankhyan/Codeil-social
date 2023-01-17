909245c92eff3f2e3655c72110a88577bc3ff9c3
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-01-17 19:44:37 +0530

    setting up jwt

--- config/passport-jwt-passport.js
+++ config/passport-jwt-passport.js
@@ -1,2 +1,22 @@
-const passport = require('passport');
-const JWTStrategy = require('passport-jwt').Strategy;
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'IIITM Gwalior';
+
+
+// using strategy
+// note user id  and password were not compared
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
