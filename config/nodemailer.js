04c2a9c1199fcdcb371644f827e0ff24ff504994
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-01-20 22:55:27 +0530

    nodemailer config

---
+++ config/nodemailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+// defines user who send mail and service which is to be used
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    host: "smtp.google.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'ankitsankhyan04@gmail.com', // generated ethereal user
+      pass: '176039', // generated ethereal password
+    },
+  });
+
+// this function will render whole HTML to give a style to sent thing
+  let renderTemplate = (data, relativepath)=>{
+    let mailHtml;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativepath),
+        data,
+        function(err, template){
+            if(err){console.log('error in rendering template');
+        return;}
+        mailHtml = template;
+        }
+    
+    )
+    return mailHtml
+  }
+
+  module.exports = {
+    transporter : transporter,
+    renderTemplate: renderTemplate
+  }
