506a7279786f24129ba191d25b239043fd557dbd
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-01-25 22:03:29 +0530

    sending events from backend related to data of user

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -21,15 +21,23 @@ class ChatEngine{
     // this method replies when a connection is established 
         connectionHandler(){
             // when connect event occurs then callback function is called
+            console.log('inside connection handler')
             let self = this;
             console.log('inside connection handler')
-            this.socket.emit('join_room', function(){
-                 
-                self.socket.emit('join_room', {
-                    User_Email: self.userEmail,
-                    chatroom:'ankit_chat_room'
-                })
-            })
            
+            this.socket.on('connect', function(){
+                console.log('connection establised using sockets...!');
+               
+                 
+                    self.socket.emit('join_room', {
+                        User_Email: self.userEmail,
+                        chatroom:'ankit_chat_room'
+                    })
+            //   this is sending the event from one thing and receiving here only
+                    self.socket.on('user_joined', function(data){
+                        console.log('user joined ', data);
+                    })
+            
+            });
         }
     }
--- config/chatsocket.js
+++ config/chatsocket.js
@@ -1,22 +1,23 @@
-module.exports.chatSockets = function(SocketServer){
+module.exports.chatSockets = function (SocketServer) {
    let io = require('socket.io')(SocketServer);
 
 
-   io.sockets.on('connection', function(socket){
-    console.log('new connection received', socket.id);
+   io.sockets.on('connection', function (socket) {
+      console.log('new connection received', socket.id);
 
-    // for disconnection Note funtion is present in socket
-   socket.on('disconnect', function(){
-    console.log('socket disconnected');
-   });
-    socket.join('join_room', function(data){
-        console.log('joining room', data );
-// if chatroom is present then that will be joined or created at the moment
-        socket.join(data.chatroom);
+      // for disconnection Note funtion is present in socket
+      socket.on('disconnect', function () {
+         console.log('socket disconnected');
+      });
+      socket.on('join_room', function (data) {
+        
+         console.log('joining room', data);
+         // if chatroom is present then that will be joined or created at the moment
+         socket.join(data.chatroom);
 
-      //   sending notification in chatroom
-
-      io.in(data.chatroom).emit('user joined');
-    })
-})
+         //   sending notification in chatroom
+            socket.emit('user_joined', data);
+         io.in(data.chatroom).emit('user joined');
+      })
+   })
 }
